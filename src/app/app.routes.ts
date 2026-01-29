2fa56dd1a8f4969dbdd1ab7d3b3a465073ce01b7
Prasa100 <th3r3al3mail@gmail.com>
2026-01-29 17:07:56 +0530

    fixed pathmatch lol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,17 @@
 import { Routes } from '@angular/router';
-import {Login} from './login/login'
+import { Login } from './login/login';
 import { Signup } from './signup/signup';
 import { TransactionList } from './transaction-list/transaction-list';
 import { TransactionForm } from './transaction-form/transaction-form';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: Login
-    },
-    {
-        path: 'signup',
-        component: Signup
-    }, 
-    {
-        path: 'transactions',
-        component: TransactionList
-    },
-    {
-        path: 'add',
-        component: TransactionForm
-    },
-    {
-        path:'edit/:id',
-        component: TransactionForm
-    },
-    {
-        path:'**',
-        redirectTo: '/transactions',
-        pathMatch: 'full'
-    },
-    {
-        path:'',
-        redirectTo: '/transactions'
-    }
+  { path: '', redirectTo: 'transactions', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'transactions', component: TransactionList },
+  { path: 'add', component: TransactionForm },
+  { path: 'edit/:id', component: TransactionForm },
+
+  { path: '**', redirectTo: 'transactions' }
 ];
